03e619178943ed6b417fce82049c04d9c34ea740
pcp16 <pcp16@users.noreply.github.com>
2022-01-14 13:37:22 +0100

    Removed commented code.
    Removed commented code, and changed name of generated data file.

--- HAR_main.m
+++ HAR_main.m
@@ -16,28 +16,4 @@ end
 [WFeat, Name, Weka, AllFeatures] = HAR_Features (file_list); 
 
 %%FILE .ARFF for Weka
-arffwrite_HAR('HAR_Weka_Pau+Angela_52', AllFeatures, Name)
-
-%% ________________PLOTS______________%%
-
-%if (0)
-      
-%     assetime = 1/fs : 1/fs : ((1/fs)* length(mag_acc_m));
-%     figure;
-%     plot(assetime, mag_acc_m);
-%     ylabel('Acceleration (m/s^2)');
-%     xlabel('Samples');
-%     
-%     figure;
-%     plot(acc_x,'r')
-%     plot(assetime, acc_x)
-%     hold on
-%     plot(acc_y,'b')
-%     plot(assetime, acc_y)
-%     hold on
-%     plot(acc_z, 'k');
-%     plot(assetime, acc_z);
-%     xlabel('Samples');
-%     ylabel ('Acceleration (m/s^2)');
-%     legend ('Acc_x','Acc_y','Acc_z')
-%     end
+arffwrite_HAR('HAR_52participants', AllFeatures, Name)
